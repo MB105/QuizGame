1c90e58fec71e7b6a03fd31c6d671b62f201f037
Marwa El Mounir <cph-me319@cphbusiness.dk>
2025-02-13 12:07:42 +0100

    update stricpt

--- Assets/DoorTrigger.cs
+++ Assets/DoorTrigger.cs
@@ -6,14 +6,22 @@ public class DoorTrigger : MonoBehaviour
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); // Henter Animator-komponenten på døren
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // Sørg for at din player har tag "Player"
+        if (other.CompareTag("Player")) // Tjekker om det er spilleren, der træder ind
         {
-            animator.SetTrigger("Open");
+            animator.SetTrigger("Open"); // Starter åbne-animationen
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // Tjekker om spilleren forlader zonen
+        {
+            animator.SetTrigger("Close"); // Starter lukke-animationen
         }
     }
 }
